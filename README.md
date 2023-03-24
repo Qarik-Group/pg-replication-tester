cfc68ecf959305308cfe33e22a69209b8c98d714
Mariusz Olejnik <mariusz.olejnik@infinite.pl>
2023-03-24 11:56:08 +0100

    v.1.1.0 fixes, support for postgres 15.2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Postgres Replication Tester
 ===========================
 
 This repository houses `pgrt`, a small utility that will connect
-to all of the nodes in a PostgreSQL streaming replication cluster
+to all the nodes in a PostgreSQL streaming replication cluster
 and verify the health and well-being of each node.
 
 Example
@@ -11,10 +11,11 @@ Example
 A healthy cluster, with some leeway on the replication lag (`-l)
 
 ```
-$ pgrt -M 10.244.232.2 -S 10.244.232.3 -S 10.244.232.4 -l 31768
-10.244.232.2: 0/B30BA98
-10.244.232.3: 0/B30BA98 (0)            to 0/B30BC28 (-400)
-10.244.232.4: 0/B30BDC0 (-808)         to 0/B30BDC0 (0)
+$ pgrt -M 10.244.232.2 -S 10.244.232.3 -S 10.244.232.4 -l 65536
+
+[2023-03-24T10:37:17Z] Master 10.244.232.2 current wal LSN 731586560 (0/2B9B2000)
+[2023-03-24T10:37:17Z] Slave 10.244.232.3(true) receives wals with lag 0, replays received wals with lag 72 - max accepted lag 65536 - keeps up with master
+[2023-03-24T10:37:18Z] Slave 10.244.232.4(true) receives wals with lag 592, replays received wals with lag 0 - max accepted lag 65536 - keeps up with master
 ```
 
 The same cluster, reporting as unhealthy because we only tolerate
@@ -22,31 +23,59 @@ The same cluster, reporting as unhealthy because we only tolerate
 
 ```
 $ pgrt -M 10.244.232.2 -S 10.244.232.3 -S 10.244.232.4 -l 800
-10.244.232.2: 0/B17F098
-10.244.232.3: 0/B17F230 (-408)         to 0/B17F230 (0)
-10.244.232.4: 0/B17F558 (-1216)        to 0/B17F6E8 (-400)          !! too far behind write master
-FAILED
+
+[2023-03-24T10:43:51Z] Master 10.244.232.2 current wal LSN 798990336 (0/2F9FA000)
+[2023-03-24T10:43:50Z] Slave 10.244.232.3(true) receives wals with lag 0, replays received wals with lag 56 - max accepted lag 800 - keeps up with master
+[2023-03-24T10:43:51Z] Slave 10.244.232.4(true) receives wals with lag 840, replays received wals with lag 0 - max accepted lag 800 - is too far behind write master
+[2023-03-24T10:43:51Z] FAILED (lagging slaves 1)
+
 ```
+
+Debugging:
+```
+$ pgrt -M 10.244.232.2 -S 10.244.232.3 -l 800 --debug
+
+[2023-03-24T10:46:11Z] DEBUG> connecting to `host=10.244.232.2 port=6432 ...`
+[2023-03-24T10:46:11Z] DEBUG> query: `SELECT pg_is_in_recovery()`
+[2023-03-24T10:46:11Z] DEBUG> query result: `false`
+[2023-03-24T10:46:11Z] DEBUG> query: `SELECT pg_current_wal_lsn()`
+[2023-03-24T10:46:11Z] DEBUG> query result: `0/3137E000`
+[2023-03-24T10:46:11Z] DEBUG> connecting to `host=10.244.232.3 port=6432 ...`
+[2023-03-24T10:46:11Z] DEBUG> query: `SELECT pg_is_in_recovery()`
+[2023-03-24T10:46:11Z] DEBUG> query result: `true`
+[2023-03-24T10:46:11Z] DEBUG> query: `SELECT pg_last_wal_receive_lsn()`
+[2023-03-24T10:46:11Z] DEBUG> query result: `0/31000000`
+[2023-03-24T10:46:11Z] DEBUG> query: `SELECT pg_last_wal_replay_lsn()`
+[2023-03-24T10:46:11Z] DEBUG> query result: `0/3112B198`
+[2023-03-24T10:46:11Z] Master 10.244.232.2 current wal LSN 825745408 (0/3137E000)
+[2023-03-24T10:46:11Z] DEBUG> calculate lag between master 10.244.232.2 and slave 10.244.232.3:
+  master.currentWalLsn    = 825745408 (0/3137E000)
+  slave.lastWalReceiveLsn = 822083584 (0/31000000)
+  slave.lastWalReplayLsn  = 823308696 (0/3112B198)
+  slave.receiveLag        = 3661824
+  slave.replayLag         = 0
+[2023-03-24T10:46:11Z] Slave 10.244.232.3(true) receives wals with lag 3661824, replays received wals with lag 0 - max accepted lag 800 - is too far behind write master
+[2023-03-24T10:46:11Z] FAILED (lagging slaves 1)
+```
+
 
 Exit Codes
 ----------
 
 `pgrt` exits 0 if it can contact all nodes, each node is playing
-the part specified (i.e. write master is a write master, and read
-slaves are actually read slaves), and the replication lag (first
+the part specified (i.e. write master is a writing master, and read
+slaves are actually reading slaves), and the replication lag (first
 parenthetical figure) is below the acceptable lag (per `-l`)
 
 It exists non-zero on failure, with the following meanings:
 
-- **1** - Option processing or other non-runtime error.
-          Check your flags.
+- **1** - Option processing or other non-runtime error. Check your flags.
 - **2** - Connectivity to at least one node failed.
 - **3** - A query to the write master failed
 - **4** - A query to one of the read slaves failed
-- **5** - xlog conversion failed (if this happens, something is
-          terribly broken...)
-- **6** - One or more of the read slaves was lagging too far
-          behind the master (based on `-l`)
+- **5** - pg_lsn conversion failed (if this happens, something is terribly broken...)
+- **6** - One or more of the read slaves was lagging too far behind the master (based on `-l`)
+- **7** - the master is in recovery mode or some slaves aren't in recovery mode
 
 Options
 -------
@@ -55,12 +84,13 @@ Options
 -M, --master   Replication master host.  May only be specified once
 -S, --slave    Replication slave host(s).  May be specified more than once
 -p, --port     TCP port that Postgres listens on (default: 6432)
--d, --database Database to use for testing
 -u, --user     User to connect as
 -w, --password Password to connect with
+-d, --database Database to use for testing, default: same as the User
+-l, --lag      Maximum acceptable slave receive and replay lag (default: 8192)
 -D, --debug    Enable debugging output (to standard error)
--l, --lag      Maximum acceptable lag behind the master xlog position (bytes) (default: 8192)
--v, --version  Program version
+-v, --version  Output version information, then exit
+-h, --help     Show this help, then exit
 ```
 
 Building
--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/voxelbrain/goptions"
 	"os"
 	"strconv"
 	"strings"
-
-	_ "github.com/lib/pq"
-	"github.com/voxelbrain/goptions"
 )
 
 const (
@@ -20,18 +19,11 @@ const (
 	BecauseSlaveQueryFailed    = 4
 	BecauseWalConversionFailed = 5
 	BecauseReplicationLag      = 6
+	BecauseWrongInRecovery     = 7
 )
 
-var debugging = false
-
-func debug(f string, a ...interface{}) {
-	if debugging {
-		fmt.Printf("DEBUG> "+f+"\n", a...)
-	}
-}
-
 func query1(db *sql.DB, q string) (string, error) {
-	debug("query: `%s`", q)
+	log.debug("query: `%s`", q)
 	r, err := db.Query(q)
 	if err != nil {
 		return "", err
@@ -43,19 +35,18 @@ func query1(db *sql.DB, q string) (string, error) {
 		return "", err
 	}
 
-	debug("query result: `%s`", v)
+	log.debug("query result: `%s`", v)
 	return v, nil
 }
 
 func connect(host, port, user, password, dbname string) *sql.DB {
 	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	debug("connecting to `%s`", conn)
+	log.debug("connecting to `%s`", conn)
 
 	db, err := sql.Open("postgres", conn)
 	if err != nil {
-		fmt.Printf("Error connecting to %s:%s as user %s, database %s: %s\n",
-			host, port, user, dbname, err)
+		log.error("Error connecting to %s:%s as user %s, database %s: %s", host, port, user, dbname, err)
 		os.Exit(BecauseConnectionFailed)
 		return nil
 	}
@@ -68,86 +59,113 @@ func parsePgLsn(s string) uint64 {
 	const supportInfo = "parser supports values between 0/0 and FFFFFFFF/FFFFFFFF"
 	l := strings.SplitN(s, "/", 3)
 	if len(l) != 2 {
-		fmt.Fprintf(os.Stderr, "parsing pg_lsn=`%s` failed (not two parts?), %s\n", s, supportInfo)
+		log.error("parsing pg_lsn=`%s` failed (not two parts?), %s", s, supportInfo)
 		os.Exit(BecauseWalConversionFailed)
 	}
 
 	a, err := strconv.ParseUint(l[0], 16, 64)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "parsing pg_lsn=`%s` failed (first part), %s\n", s, supportInfo)
+		log.error("parsing pg_lsn=`%s` failed (first part), %s", s, supportInfo)
 		os.Exit(BecauseWalConversionFailed)
 	}
 
 	b, err := strconv.ParseUint(l[1], 16, 64)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "parsing pg_lsn=`%s` failed (second part), %s\n", s, supportInfo)
+		log.error("parsing pg_lsn=`%s` failed (second part), %s", s, supportInfo)
 		os.Exit(BecauseWalConversionFailed)
 	}
 
 	return a<<32 + b
 }
 
+func queryIsInRecovery(db *sql.DB) bool {
+	var isInRecoveryStr string
+	var err error
+	if isInRecoveryStr, err = query1(db, "SELECT pg_is_in_recovery()"); err != nil {
+		log.error("Failed to query recovery mode: %s", err)
+		os.Exit(BecauseMasterQueryFailed)
+	}
+	return isInRecoveryStr == "t" || isInRecoveryStr == "true"
+}
+
 type Master struct {
-	name          string
-	currentWalLsn string // lsn - Log Sequence Number
+	name             string
+	isInRecovery     bool
+	currentWalLsn    string // lsn - Log Sequence Number
+	currentWalLsnInt uint64
 }
 
 func QueryMaster(host, port, user, pass, dbname string) (m Master) {
 	m.name = host
 	db := connect(host, port, user, pass, dbname)
 	defer db.Close()
+	m.isInRecovery = queryIsInRecovery(db)
 	var err error
 	// https://www.postgresql.org/docs/current/functions-admin.html
 	// pg_current_xlog_location() -> pg_current_wal_lsn()
 	if m.currentWalLsn, err = query1(db, "SELECT pg_current_wal_lsn()"); err != nil {
-		fmt.Printf("Failed to query current wal location: %s\n", err)
+		log.error("Failed to query current wal location: %s", err)
+		// TODO: err when is a slave: Failed to query current wal location: pq: recovery is in progress
 		os.Exit(BecauseMasterQueryFailed)
 	}
+	m.currentWalLsnInt = parsePgLsn(m.currentWalLsn)
 	return
 }
 
 type Slave struct {
-	name              string
-	lastWalReceiveLsn string
-	lastWalReplayLsn  string
+	name                 string
+	isInRecovery         bool
+	lastWalReceiveLsn    string
+	lastWalReceiveLsnInt uint64
+	lastWalReplayLsn     string
+	lastWalReplayLsnInt  uint64
 }
 
 func QuerySlave(host, port, user, pass, dbname string) (s Slave) {
 	s.name = host
 	db := connect(host, port, user, pass, dbname)
 	defer db.Close()
+	s.isInRecovery = queryIsInRecovery(db)
 	var err error
 	// https://www.postgresql.org/docs/current/functions-admin.html
 	// pg_last_xlog_receive_location() -> pg_last_wal_receive_lsn()
 	// pg_last_xlog_replay_location() -> pg_last_wal_replay_lsn()
 	// pg_xlog_location_diff() ->  pg_wal_lsn_diff()
 	if s.lastWalReceiveLsn, err = query1(db, "SELECT pg_last_wal_receive_lsn()"); err != nil {
-		fmt.Printf("Failed to query last received wal location: %s\n", err)
+		log.error("Failed to query last received wal location: %s", err)
 		os.Exit(BecauseSlaveQueryFailed)
 	}
+	s.lastWalReceiveLsnInt = parsePgLsn(s.lastWalReceiveLsn)
 
 	if s.lastWalReplayLsn, err = query1(db, "SELECT pg_last_wal_replay_lsn()"); err != nil {
-		fmt.Printf("Failed to query last replayed wal location: %s\n", err)
+		log.error("Failed to query last replayed wal location: %s", err)
 		os.Exit(BecauseSlaveQueryFailed)
 	}
+	s.lastWalReplayLsnInt = parsePgLsn(s.lastWalReplayLsn)
 	return
 }
 
-func (s *Slave) CalculateLag(m Master) (behind uint64, delay uint64) {
-	debug("checking: master.currentWalLsn=`%s`, slave.lastWalReceiveLsn=`%s`, slave.lastWalReplayLsn=`%s`", m.currentWalLsn, s.lastWalReceiveLsn, s.lastWalReplayLsn)
-	masterCurrentWalLsn := parsePgLsn(m.currentWalLsn)
-	slaveLastWalReceive := parsePgLsn(s.lastWalReceiveLsn)
-	slaveLastWalReplay := parsePgLsn(s.lastWalReplayLsn)
-	behind = 0
-	if masterCurrentWalLsn > slaveLastWalReceive {
-		behind = masterCurrentWalLsn - slaveLastWalReceive
+func (s *Slave) CalculateLag(m Master) (receiveLag uint64, replayLag uint64) {
+	receiveLag = 0
+	if m.currentWalLsnInt > s.lastWalReceiveLsnInt {
+		receiveLag = m.currentWalLsnInt - s.lastWalReceiveLsnInt
 	}
-	delay = 0
-	if slaveLastWalReceive > slaveLastWalReplay {
-		delay = slaveLastWalReceive - slaveLastWalReplay
+	replayLag = 0
+	if s.lastWalReceiveLsnInt > s.lastWalReplayLsnInt {
+		replayLag = s.lastWalReceiveLsnInt - s.lastWalReplayLsnInt
 	}
-	debug("checking: master.currentWalLsn=%d, slave.lastWalReceive=%d, slave.lastWalReplay=%d, slave.behind=%d, slave.delay=%d", masterCurrentWalLsn, slaveLastWalReceive, slaveLastWalReplay, behind, delay)
-	return behind, delay
+	log.debug("calculate lag between master %s and slave %s:\n"+
+		"  master.currentWalLsn    = %d (%s)\n"+
+		"  slave.lastWalReceiveLsn = %d (%s)\n"+
+		"  slave.lastWalReplayLsn  = %d (%s)\n"+
+		"  slave.receiveLag        = %d\n"+
+		"  slave.replayLag         = %d",
+		m.name, s.name,
+		m.currentWalLsnInt, m.currentWalLsn,
+		s.lastWalReceiveLsnInt, s.lastWalReceiveLsn,
+		s.lastWalReplayLsnInt, s.lastWalReplayLsn,
+		receiveLag, replayLag)
+	return receiveLag, replayLag
 }
 
 func main() {
@@ -155,11 +173,11 @@ func main() {
 		Master    string   `goptions:"-M, --master, description='Replication master host.  May only be specified once'"`
 		Slaves    []string `goptions:"-S, --slave, description='Replication slave host(s).  May be specified more than once'"`
 		Port      string   `goptions:"-p, --port, description='TCP port that Postgres listens on'"`
-		Database  string   `goptions:"-d, --database, description='Database to use for testing'"`
 		User      string   `goptions:"-u, --user, description='User to connect as'"`
 		Password  string   `goptions:"-w, --password, description='Password to connect with'"`
+		Database  string   `goptions:"-d, --database, description='Database to use for testing, default: same as the User'"`
+		AcceptLag int64    `goptions:"-l, --lag, description='Maximum acceptable slave receive and replay lag (bytes)'"`
 		Debug     bool     `goptions:"-D, --debug, description='Enable debugging output (to standard error)'"`
-		AcceptLag int64    `goptions:"-l, --lag, description='Maximum acceptable lag behind the master wal position (bytes)'"`
 		Version   bool     `goptions:"-v, --version, description='Output version information, then exit'"`
 		Help      bool     `goptions:"-h, --help, description='Show this help, then exit'"`
 	}{
@@ -179,33 +197,42 @@ func main() {
 	if options.Database == "" {
 		options.Database = options.User
 	}
-	debugging = options.Debug
+	log.debugging = options.Debug
 
+	master := QueryMaster(options.Master, options.Port, options.User, options.Password, options.Database)
+	if master.isInRecovery {
+		log.error("FAILED (master %s is in recovery: %t)", master.name, master.isInRecovery)
+		os.Exit(BecauseWrongInRecovery)
+	}
 	var slaves = make([]Slave, len(options.Slaves))
 	for i, host := range options.Slaves {
-		slaves[i] = QuerySlave(host, options.Port,
-			options.User, options.Password, options.Database)
+		slaves[i] = QuerySlave(host, options.Port, options.User, options.Password, options.Database)
 	}
 
-	master := QueryMaster(options.Master, options.Port,
-		options.User, options.Password, options.Database)
-
-	fmt.Printf("%s: %s\n", master.name, master.currentWalLsn)
-	failed := false
+	log.info("Master %s current wal LSN %d (%s)", master.name, master.currentWalLsnInt, master.currentWalLsn)
+	laggingSlavesCount := 0
+	notInRecoverySlavesCount := 0
 	for _, slave := range slaves {
-		behind, delay := slave.CalculateLag(master)
-
-		emsg := ""
-		if behind > uint64(options.AcceptLag) {
-			failed = true
-			emsg = "    !! too far behind write master"
+		receiveLag, replayLag := slave.CalculateLag(master)
+		if !slave.isInRecovery {
+			notInRecoverySlavesCount++
 		}
 
-		fmt.Printf("%s: %s (behind %d) to %s (delay %d) %s\n", slave.name,
-			slave.lastWalReceiveLsn, behind, slave.lastWalReplayLsn, delay, emsg)
+		emsg := "keeps up with master"
+		if receiveLag > uint64(options.AcceptLag) || replayLag > uint64(options.AcceptLag) {
+			laggingSlavesCount++
+			emsg = "is too far behind write master"
+		}
+
+		log.info("Slave %s(%t) receives wals with lag %d, replays received wals with lag %d - max accepted lag %d - %s",
+			slave.name, slave.isInRecovery, receiveLag, replayLag, options.AcceptLag, emsg)
 	}
-	if failed {
-		fmt.Print("FAILED\n")
+	if notInRecoverySlavesCount > 0 {
+		log.error("FAILED (some slaves aren't in recovery: %d)", master.isInRecovery, notInRecoverySlavesCount)
+		os.Exit(BecauseWrongInRecovery)
+	}
+	if laggingSlavesCount > 0 {
+		log.error("FAILED (lagging slaves %d)", laggingSlavesCount)
 		os.Exit(BecauseReplicationLag)
 	}
 }
